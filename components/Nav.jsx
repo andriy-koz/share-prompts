b0abb83bcb060eb9f7f2a4cc0e728a7abe237ac7
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-06-03 17:41:58 -0300

    finishing auth

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -11,12 +11,12 @@ const Nav = () => {
   const [toggleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders()
       setProviders(response)
     }
 
-    setProviders()
+    setUpProviders()
   }, [])
 
   return (
